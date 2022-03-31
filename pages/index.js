425b3619028eae0881b8568f1ee62f26335ee676
Vishwajeetsinh Rathod <satyajeetsinh_rathod@MacBook-Air.local>
2022-03-31 22:48:34 +0530

    initial commit from  m1-air

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import { Heading, Page } from "@shopify/polaris";
+
+export default function Index() {
+  return (
+    <Page>
+      <Heading>
+        Shopify app with Nde and angular
+         React{" "}
+        <span role="img" aria-label="tada emoji">
+          🎉
+        </span>
+      </Heading>
+      
+      
+    </Page>
+  );
+}
